d783e9dd99a3a8599680ebb8baccf5023ec6cc2b
Albie007 <albinsaju1059k@gmail.com>
2026-06-25 13:22:28 +0530

    JAVA_LAB1 TASK 1

---
+++ src/CountNumbers.java
@@ -0,0 +1,24 @@
+public class CountNumbers {
+    public static void main(String[] args) {
+
+        int[] arr = {5, -3, 0, 8, -1, 4};
+
+        int positive = 0;
+        int negative = 0;
+        int zero = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 0) {
+                positive++;
+            } else if (arr[i] < 0) {
+                negative++;
+            } else {
+                zero++;
+            }
+        }
+
+        System.out.println("Positive numbers = " + positive);
+        System.out.println("Negative numbers = " + negative);
+        System.out.println("Zeros = " + zero);
+    }
+}

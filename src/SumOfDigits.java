b1c4994913bc48be2bbc47f6f5933f84ecd7d8fe
Albie007 <albinsaju1059k@gmail.com>
2026-07-01 08:38:06 +0530

    cycle1task15javalab1

---
+++ src/SumOfDigits.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SumOfDigits {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Number = ");
+        int number = scanner.nextInt();
+
+        int temp = Math.abs(number);
+        int sum = 0;
+        while (temp > 0) {
+            sum += temp % 10;
+            temp /= 10;
+        }
+
+        System.out.println("Sum of digits = " + sum);
+
+        scanner.close();
+    }
+}
